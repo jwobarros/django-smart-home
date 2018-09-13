4e0782c5aa4f51196433073935667421ba4f8eff
Johnnatan Barros <johnnatanwillian@hotmail.com>
2018-09-13 00:06:30 -0400

    Added ajax to send audio

--- SmartHome/views.py
+++ SmartHome/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
 
-    template_name = "audio.html"
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
