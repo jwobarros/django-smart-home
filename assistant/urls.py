39cb0bcc42af3300785b50b128608ad3501a84b1
Johnnatan Barros <johnnatanwillian@hotmail.com>
2018-09-12 23:38:22 -0400

    Added js

--- assistant/urls.py
+++ assistant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from assistant.views import OnOffView
+from assistant.views import OnOffView, TemperatureHumidityView, AudioView
 
 urlpatterns = [
     path('on-off', OnOffView, name='on_off'),
+    path('temperature-humidity', TemperatureHumidityView, name='temperature_humidity'),
+    path('audio', AudioView, name='audio')
 ]
--- assistant/views.py
+++ assistant/views.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from os import system
 
 from assistant.models import RadioFrequencyRelay
-# Create your views here.
+
+#Temperature and Humidity Sensor Library
+import Adafruit_DHT
+
+
 
 @csrf_exempt
 def OnOffView(request):
@@ -28,3 +32,35 @@ def OnOffView(request):
       #Power Off 
       system(RFR.off())
       return HttpResponse('Desligado')
+
+
+
+@csrf_exempt
+def TemperatureHumidityView(request):
+
+    if request.is_ajax():
+
+        sensor = Adafruit_DHT.AM2302
+        pin = 23
+
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+        if humidity is not None and temperature is not None:
+
+            data = {"humidity": round(humidity, 2), "temperature": round(temperature, 2) }
+
+            return JsonResponse(data, safe=False)
+
+
+
+
+def AudioView(request):
+
+    if request.is_ajax() and request.method == "POST":
+        audio = request.FILES['audio']
+        print(audio)
+        return JsonResponse('Foi porra', safe=False)
+    raise Http404
+
+
+
